c2cc45ebb1a8bc2652a538ad01ab1b181c7d9ff9
Rafael Liendo <rafael.n.liendo@gmail.com>
2020-10-12 21:51:43 -0300

    prevent initial_data from running twice

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
 from app.db.session import get_db
-from app.db.crud import create_user
+from app.db.crud import create_user, get_user_by_email
 from app.db.schemas import UserCreate
 from app.db.session import SessionLocal
 
 
 def init() -> None:
     db = SessionLocal()
-
-    create_user(
-        db,
-        UserCreate(
-            email="admin@ecommerce.com",
-            password="password",
-            is_active=True,
-            is_superuser=True,
-        ),
-    )
+    admin_user_exists = get_user_by_email(db, "admin@ecommerce.com")
+    if not admin_user_exists:
+        create_user(
+            db,
+            UserCreate(
+                email="admin@ecommerce.com",
+                password="password",
+                is_active=True,
+                is_superuser=True,
+            ),
+        )
 
 
 if __name__ == "__main__":
