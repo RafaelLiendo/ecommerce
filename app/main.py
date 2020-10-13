0edad7ddbe8b81d4d0ca6f9c22ccf6d3cae98174
Rafael Liendo <rafael.n.liendo@gmail.com>
2020-10-12 23:09:59 -0300

    remove unused code

--- app/main.py
+++ app/main.py
@@ -46,4 +46,4 @@ app.include_router(
 app.include_router(auth_router, prefix="/api", tags=["auth"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-docker-compose run backend pytest
-docker-compose run frontend test
+docker-compose run backend pytest $@
