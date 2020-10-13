260975fccbd688bdff1ec5823975605d8d31b01f
Rafael Liendo <rafael.n.liendo@gmail.com>
2020-10-12 21:43:27 -0300

    implement models

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DECIMAL, DateTime
+from sqlalchemy.orm import relationship
 
 from .session import Base
 
@@ -6,10 +7,48 @@ from .session import Base
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False, index=True)
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
+    cart_products = relationship("CartProduct")
+    orders = relationship("Order")
+
+class CartProduct(Base):
+    __tablename__ = "cart_product"
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True)
+    product = relationship("Product")
+    quantity = Column(Integer)
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(DECIMAL)
+
+
+class Order(Base):
+    __tablename__ = "order"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime)
+    total = Column(DECIMAL)
+    order_products = relationship("OrderProduct")
+
+class OrderProduct(Base):
+    __tablename__ = "order_product"
+
+    order_id = Column(Integer, ForeignKey('order.id'), primary_key=True)
+    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True)
+    product = relationship("Product")
+    quantity = Column(Integer)
+    subtotal = Column(DECIMAL)
